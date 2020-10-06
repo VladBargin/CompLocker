013ddc707fe1404a4c1ad249000eb452178e83df
VladBargin <55981762+VladBargin@users.noreply.github.com>
2020-10-06 15:14:31 +0300

    Update cam.py

--- scripts/cam.py
+++ scripts/cam.py
@@ -4,8 +4,8 @@ from time import sleep
 camera = PiCamera()
 
 camera.start_preview()
-for i in range(5):
-    sleep(2)
-    camera.capture("%s.jpg" % i)
+camera.start_recording("vid0.h264")
+sleep(30)
+camera.stop_recording()
 camera.stop_preview()
 
