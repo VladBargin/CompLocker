c90ca5d0737d83d2ea15ef99fd64d835f79fa22b
Vladislav Bargin <vladithur@mail.ru>
2020-10-16 13:55:07 +0300

    Line sensor test

---
+++ scripts/lines.py
@@ -0,0 +1,17 @@
+from RPi import GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+left_sensor = 11
+right_sensor = 9
+
+GPIO.setup(left_sensor, GPIO.IN)
+GPIO.setup(right_sensor, GPIO.IN)
+
+try:
+	while True:
+		print("Left sensor active:", bool(GPIO.input(left_sensor), "  Right sensor active:", bool(GPIO.input(right_sensor))
+		sleep(0.5)
+except:
+	GPIO.cleanup()
